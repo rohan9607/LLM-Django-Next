5c696686c28e22ce8e6f807ca7d355944e0018c7
rgaikwad <rgaikwad@novatech.com>
2023-10-04 01:06:03 +0530

    Done vector embeddings and created conversation store

--- backend/backend/llm/mainApp/app.py
+++ backend/backend/llm/mainApp/app.py
@@ -1,8 +1,17 @@
 # imports 
 from PyPDF2 import PdfReader
-
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceBgeEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
+from langchain.agents import initialize_agent
 
 # get the chunks of uploaded pfd files
+
+chain = None
 
 def get_pdf_text(pdf_files):
     text = ""
@@ -13,3 +22,32 @@ def get_pdf_text(pdf_files):
             
     return text
 
+# Split the text into chunks
+def make_chunks_of_raw_text(rawText):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+        )
+    return text_splitter.split_text(rawText)
+
+def create_vectorstore(chunksOfText):
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(texts=chunksOfText, embedding=embeddings)
+    return vectorstore
+
+def create_conversation_chain(vectorstore):
+    global chain
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.2, "max_length":512})
+    llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo-0301')
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True, output_key='answer' , input_key="question")
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+
+    # print(conversation_chain.run({"question" : "what is Mucous membrane?"}))
+    return conversation_chain
--- backend/backend/llm/views.py
+++ backend/backend/llm/views.py
@@ -6,10 +6,11 @@ from rest_framework.views import APIView
 from rest_framework import status
 from dotenv import load_dotenv
 import os
-from .mainApp.app import get_pdf_text
+from .mainApp.app import get_pdf_text, make_chunks_of_raw_text, create_vectorstore, create_conversation_chain
 
 load_dotenv()
 # ViewSets define the view behavior.
+chain = None
 class UploadViewSet(ViewSet):
     serializer_class = UploadSerializer
 
@@ -17,11 +18,25 @@ class UploadViewSet(ViewSet):
         return Response("GET API")
 
     def create(self, request):
+        global chain
         files = request.FILES.getlist('file')
         
         # Extract all text from PDF
         raw_text = get_pdf_text(files)
-        return JsonResponse({"raw" : raw_text}, safe=False)
+
+        # Make chunks of raw text
+        chunks = make_chunks_of_raw_text(rawText=raw_text)
+
+        # create vector store
+        vectorStore = create_vectorstore(chunks)
+
+        # Create chat session for conversation
+        chain = create_conversation_chain(vectorStore)
+        if(chain != ""):
+            return Response({'message': "Conversation Chain Created", "success" : True}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message" : "Something went wrong","success" : False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
 
 class QuestionView(APIView):
        def post(self, request, *args, **kwargs):
@@ -29,7 +44,10 @@ class QuestionView(APIView):
         token = os.environ.get("HUGGING_FACE_TOKEN")
         if serializer.is_valid():
             question = serializer.validated_data['question']
-            return Response({'message': question, "token" : token}, status=status.HTTP_201_CREATED)
+            response = chain.run({'question': question})
+            # ans = response['chat_history']
+            # print({ans})
+            return Response({'message': question, "chatHistory" : response}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
